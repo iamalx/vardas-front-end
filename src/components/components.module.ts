8b43eaf65c40e25bc4de5402373194666838b0fd
Alex <reyesgx@gmail.om>
2018-12-11 14:30:37 -0800

    test

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { HeartBreathComponent } from './heart-breath/heart-breath';
+@NgModule({
+	declarations: [HeartBreathComponent],
+	imports: [],
+	exports: [HeartBreathComponent]
+})
+export class ComponentsModule {}
