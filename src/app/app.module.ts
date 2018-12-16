c015848dac3df1004051a7e46366ba64a708c251
Alex <reyesgx@gmail.om>
2018-12-16 02:10:56 -0800

    intall apk to deviece and livereload to device; used initialized and enable() successfully => use it to pair with device

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { BluetoothLE } from '@ionic-native/bluetooth-le';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -26,6 +27,7 @@ import { HeartBreathComponent } from '../components/heart-breath/heart-breath'
   ],
   providers: [
     StatusBar,
+    BluetoothLE,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
--- src/components/heart-breath/heart-breath.ts
+++ src/components/heart-breath/heart-breath.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BluetoothLE } from '@ionic-native/bluetooth-le';
+import { Platform } from 'ionic-angular';
 
 /**
  * Generated class for the HeartBreathComponent component.
@@ -6,6 +8,7 @@ import { Component } from '@angular/core';
  * See https://angular.io/api/core/Component for more info on Angular
  * Components.
  */
+declare var cordova: any;
 @Component({
   selector: 'heart-breath',
   templateUrl: 'heart-breath.html'
@@ -14,9 +17,20 @@ export class HeartBreathComponent {
 
   text: string;
 
-  constructor() {
-    console.log('Hello HeartBreathComponent Component');
+  constructor(  public bluetoothle: BluetoothLE,
+                public plt: Platform) {
+   // console.log('Hello HeartBreathComponent ');
     this.text = 'Hello World!!!!';
-  }
 
+    this.plt.ready().then((readySource) => {
+      this.text = readySource;
+      this.bluetoothle.initialize().then(blue => {
+        this.text = blue.status // logs 'enabled'
+        if(blue.status == 'disabled') {
+          this.bluetoothle.enable()     
+        }
+      }); 
+    });
+  }
 }
+
