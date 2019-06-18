0e75aa7ca6f9451187e0c410416e8f7d21d8675c
Alex <reyesgx11@gmail.com>
2019-06-18 15:21:34 -0700

    cleaning code, scanned zone adress => connect, read and write

--- src/components/heart-breath/heart-breath.ts
+++ src/components/heart-breath/heart-breath.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { BluetoothLE } from '@ionic-native/bluetooth-le';
 import { Platform } from 'ionic-angular';
+import { stringify } from '@angular/core/src/render3/util';
 /**
  * Generated class for the HeartBreathComponent component.
  *
@@ -14,100 +15,118 @@ declare var cordova: any;
 })
 export class HeartBreathComponent {
 
-  text: string;
+  text: [string] = ['Hello World!'];
 
   constructor(  public bluetoothle: BluetoothLE,
                 public plt: Platform ) {
-   // console.log('Hello HeartBreathComponent ');
-    this.text = 'Hello World!!!!';
-
     this.plt.ready().then( readySource => {
-      this.text = readySource;
-      //let initializeResult: object;
-      let params: object = {
-        "request": true,
-        "statusReceiver": false,
-        "restoreKey": "bluetoothleplugin"
-      };
+      this.text.push(readySource)
+      this.initialize()
+    });
+  }
 
-      this.bluetoothle.initialize( params ).then( blue => {
-        this.text = blue.status // logs 'enabled'
-        console.log(blue.status, "#3")
-      });
+  initialize() {
+    let params: object = {
+      "request": true,
+      "statusReceiver": false,
+      "restoreKey": "bluetoothleplugin"
+    };
+    this.bluetoothle.initialize( params ).then( blue => {
+      this.text.push(blue.status) // logs 'enabled'
+      console.log(blue.status, "#3")
+      this.scan()
+    });
+  }
 
-      //connect to device to see clallback 
-      const info = this.bluetoothle.getAdapterInfo()
+  scan() {
+    let scanParams: {
+      "services": [null],
+      "allowDuplicates": false,
+      // "scanMode": bluetoothle.SCAN_MODE_LOW_LATENCY,
+      // "matchMode": bluetoothle.MATCH_MODE_AGGRESSIVE,
+      // "matchNum": bluetoothle.MATCH_NUM_MAX_ADVERTISEMENT,
+      // "callbackType": bluetoothle.CALLBACK_TYPE_ALL_MATCHES,
+    }
 
-      for( var i in info ) {
-        console.log(i, "#4") 
-      }
-
-      let scanParams:{
-        "services": [],
-        "allowDuplicates": true,
-        "scanMode": bluetoothle.SCAN_MODE_LOW_LATENCY,
-        "matchMode": bluetoothle.MATCH_MODE_AGGRESSIVE,
-        "matchNum": bluetoothle.MATCH_NUM_MAX_ADVERTISEMENT,
-        "callbackType": bluetoothle.CALLBACK_TYPE_ALL_MATCHES,
-      }
-
-      this.bluetoothle.startScan( scanParams ).subscribe( any => {
-        console.log(any.status, '#5')
-          if( any['name']) {
-            console.log(any['name'], 'advert' ,"#5.5")   
-            // console.log(any[ "advertisement"]["serviceUuids"], 'uuids' ,"#5.5")
-            // console.log(any[ "isConnectable"], 'connectable?' ,"#5.5")
-            // console.log(any["serviceData"], 'serviceData' ,"#5.5")
-            // console.log(any['name'], 'name' ,"#5.5")
-          }
-        })
-
-      
-
-      setTimeout( _ => {
-        this.bluetoothle.stopScan().then( any => {
-          console.log(any.status, "#6")
-          // for( let i in any) {
-          //   console.log(any[i], "#6.5")
-          // }
-        })
-      }, 7000 )
-
-      let connectParams = {
-        "address": "E0:F5:3C:B6:DE:5A"
-      }
-
-      let discoverParams: {
-        "address": "E0:F5:3C:B6:DE:5A",
-        "services": []
-      }
-
-      // this.bluetoothle.disconnect(connectParams).then( any => { 
-      //   console.log(any, any.status, "#11")
-      // })
-
-      this.bluetoothle.connect( connectParams ).subscribe( any => {
-        console.log(any.status, "#7")
-        console.log(Object.keys(any), '"#7.2')
-        for( let i in any) {
-          console.log(any[i], "#7.5")
+    this.bluetoothle.startScan( scanParams ).subscribe( any => {
+      console.log(any.status, '#5')
+        if( any['name']) {
+          console.log(any['name'], JSON.stringify(any) ,"#5.5")   
         }
-
-        this.bluetoothle.services(discoverParams).then( any => {
-          console.log(any, 'any', '#8')
-        
-        }, error => {
-          console.log(error.message, 'error8.2')
-        })
-
-      }, error => {
-          console.log(error.message, 'error', '#7.6')
-          this.bluetoothle.close(connectParams).then( data => console.log(data.status, 'closed', "#7.7"))
       })
 
-      // this.bluetoothle.characteristics()
+    setTimeout( _ => {
+      this.bluetoothle.stopScan().then( any => {
+        console.log(any.status, "#6")
+        this.connect()
+      })
+    }, 3000 )
+  }
 
-    });
+  connect() {
+    let connectParams = {
+      "address": "E0:F5:3C:B6:DE:5A"
+    }
+    this.bluetoothle.connect( connectParams ).subscribe( any => {
+      console.log(any.status, "#7")
+      console.log(Object.keys(any), '"#7.2')
+      for( let i in any) {
+        console.log(any[i], "#7.5")
+      }
+    }, error => {
+      console.log(error.message, 'error', '#7.6')
+      this.bluetoothle.close(connectParams).then( data => console.log(data.status, 'closed', "#7.7"))
+    }) 
+  }
+
+
+  holder() {
+
+    let discoverParams = {
+      "address": "E0:F5:3C:B6:DE:5A",
+      "clearCache": true
+    }
+
+    const readParams = {
+      "address": "E0:F5:3C:B6:DE:5A",
+      "service": '1800',
+      "characteristic": '2A00',
+    }
+
+    // let pams = {
+    //   address: "E0:F5:3C:B6:DE:5A",
+    //   service: '180D',
+    //   characteristic: "2A37"
+    // }
+    // this.bluetoothle.disconnect(connectParams).then( any => { 
+    //   console.log(any, any.status, "#11")
+    // })
+
+      this.bluetoothle.discover(discoverParams).then( any => {
+        console.log(any['status'], 'any.status', '#8')
+        console.log(JSON.stringify(any), '#8.1')
+      }, error => {
+        console.log(error.message, error, 'error8.2')
+      })
+
+      this.bluetoothle.read(readParams).then( any => {
+        console.log(JSON.stringify(any), "#9")
+      }, error => { 
+        console.log(JSON.stringify(error), 'error9.2')
+      })
+
+      // this.bluetoothle.subscribe( pams).subscribe( data => {
+      //   console.log(JSON.stringify(data), '#10')
+      // }, error => {
+      //   console.log(error.message, error, 'error#10.2')
+      // });
+      // this.bluetoothle.read(readParams).then( any => {
+      //   console.log(any, any.services)
+      // }, error => {
+      //   console.log(error.message, error, 'error8.2')
+      // })
+
+
   }
 }
 
